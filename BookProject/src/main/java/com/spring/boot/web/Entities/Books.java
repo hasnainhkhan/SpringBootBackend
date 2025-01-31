ba9e50579db15c9b48ae08eeb7e3cea9047e0309
hasnainhkhan <hhk2170@gmail.com>
2025-01-31 15:22:51 +0530

    database configuration added and modified on as entity file

--- BookProject/src/main/java/com/spring/boot/web/Entities/Books.java
+++ BookProject/src/main/java/com/spring/boot/web/Entities/Books.java
@@ -1,13 +1,24 @@
 package com.spring.boot.web.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
 public class Books {
-	
-	private int bookId;
-	private String bookTitle;
-	private String bookAuthor;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private int bookId;
+    private String bookTitle;
+    private String bookAuthor;
 }
