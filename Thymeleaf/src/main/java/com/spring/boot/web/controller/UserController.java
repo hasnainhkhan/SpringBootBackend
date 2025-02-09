98973b2fc633ff0f5eedd5bd282d3134b3412d8c
hasnainhkhan <hhk2170@gmail.com>
2025-02-09 16:41:46 +0530

    ;

--- Thymeleaf/src/main/java/com/spring/boot/web/controller/UserController.java
+++ Thymeleaf/src/main/java/com/spring/boot/web/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.spring.boot.web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class UserController {
     
-    @GetMapping("/userhome")
+    @RequestMapping(value = "/about",method = RequestMethod.GET)
     public String userHome() {
-	return "Hello this is home page";
+	return "";
     }
 }
