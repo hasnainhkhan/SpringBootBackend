074c4df24ab94a38d7643d67cb70d8642ce9730b
hasnainhkhan <hhk2170@gmail.com>
2025-02-09 17:32:39 +0530

    ;

--- Thymeleaf/src/main/java/com/spring/boot/web/controller/UserController.java
+++ Thymeleaf/src/main/java/com/spring/boot/web/controller/UserController.java
@@ -3,12 +3,15 @@ package com.spring.boot.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 
 @Controller
 public class UserController {
     
     @RequestMapping(value = "/about",method = RequestMethod.GET)
-    public String userHome() {
-	return "";
+    public String userHome(Model model) {
+	
+	model.addAttribute("name","Hasnain");	
+	return "About.html";
     }
 }
