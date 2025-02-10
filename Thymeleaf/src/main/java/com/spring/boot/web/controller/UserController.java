6769d82c9caccaa6b78ef11c613de6e48bb2a121
hasnainhkhan <hhk2170@gmail.com>
2025-02-10 22:17:58 +0530

    Some File are added in repo

--- Thymeleaf/src/main/java/com/spring/boot/web/controller/UserController.java
+++ Thymeleaf/src/main/java/com/spring/boot/web/controller/UserController.java
@@ -5,27 +5,26 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.thymeleaf.standard.expression.Each;
 
 @Controller
 public class UserController {
     
     @GetMapping("/about") // Using @GetMapping for simplicity
     public String userHome(Model model) {
-        // Create a list of users
+        // ✅ Create a list of users
+	// ✅ 
         List<String> users = new ArrayList<>();
         
         for(int i = 1; i<100; i++){
             users.add("++");
             for(int j = 1; j<2; j++) {
         	users.add("Hasnain=> " + i);
-        	
         }
         }
-
         model.addAttribute("name", "Hasnain");
         model.addAttribute("users", users); // ✅ Ensure it matches the Thymeleaf variable
-
+        model.addAttribute("1","1");
         return "About"; // Make sure you have About.html
     }
+    
 }
