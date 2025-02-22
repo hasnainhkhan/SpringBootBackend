7db3ecfb8ad54b05acda3de3bacf481355b02530
hasnainhkhan <hhk2170@gmail.com>
2025-02-22 23:20:35 +0530

    nav bar added

--- Smart_Contact_manager/src/main/java/com/smart/contact/controller/HomeController.java
+++ Smart_Contact_manager/src/main/java/com/smart/contact/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.smart.contact.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
+	model.addAttribute("title","Home~Smart Contact Manager");
 	return "home";
     }
+    
+    @RequestMapping("/about")
+    public String about(Model model) {
+	model.addAttribute("title","About~Smart Contact Manager");
+	return "about";
+    }
+    
   
 }
