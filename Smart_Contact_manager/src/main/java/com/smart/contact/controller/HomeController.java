8333a9e355cc6806216d5282bda63be03ee93b20
hasnainhkhan <hhk2170@gmail.com>
2025-02-24 01:38:55 +0530

    file are added

--- Smart_Contact_manager/src/main/java/com/smart/contact/controller/HomeController.java
+++ Smart_Contact_manager/src/main/java/com/smart/contact/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public String home(Model model){
 	model.addAttribute("title","Home~Smart Contact Manager");
 	return "home";
