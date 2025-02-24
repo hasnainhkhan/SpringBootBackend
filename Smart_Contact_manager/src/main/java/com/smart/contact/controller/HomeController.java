db92a571fb6043deb150c6e7bc9cce8b58a7664c
hasnainhkhan <hhk2170@gmail.com>
2025-02-25 02:50:43 +0530

    signup form added

--- Smart_Contact_manager/src/main/java/com/smart/contact/controller/HomeController.java
+++ Smart_Contact_manager/src/main/java/com/smart/contact/controller/HomeController.java
@@ -4,22 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
 @Controller
 public class HomeController {
-    
-    @RequestMapping("/")
-    public String home(Model model){
-	model.addAttribute("title","Home~Smart Contact Manager");
-	return "home";
-    }
-    
-    @RequestMapping("/about")
-    public String about(Model model) {
-	model.addAttribute("title","About~Smart Contact Manager");
-	return "about";
-    }
-    
-  
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "Home~Smart Contact Manager");
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About~Smart Contact Manager");
+		return "about";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("tittle","Signup~Smart Contact Manager");
+		return "signup";
+	}
+
 }
