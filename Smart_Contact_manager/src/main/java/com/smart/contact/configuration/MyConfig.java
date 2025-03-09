16957c4b404cb2e325e9e61c1c5ab5d601e48365
hasnainhkhan <hhk2170@gmail.com>
2025-03-09 23:54:43 +0530

    role base authentication added

--- Smart_Contact_manager/src/main/java/com/smart/contact/configuration/MyConfig.java
+++ Smart_Contact_manager/src/main/java/com/smart/contact/configuration/MyConfig.java
@@ -3,6 +3,7 @@ package com.smart.contact.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -40,7 +41,7 @@ public class MyConfig {
 	}
 	
 	@Bean
-	public DaoAuthenticationProvider daoAuthenticationProvider() {
+	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 		daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
 		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
@@ -49,5 +50,20 @@ public class MyConfig {
 	}
 	
 	// configuration method
-
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Bean
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	        .requestMatchers("/admin/**").hasRole("ADMIN")
+	        .requestMatchers("/user/**").hasRole("USER")
+	        .requestMatchers("/").permitAll()
+	        .and()
+	        .formLogin()
+	        .and()
+	        .csrf().disable();
+	}
 }
