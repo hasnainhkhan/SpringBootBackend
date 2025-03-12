60b86eda6347902fecba94469fec210696d4f365
hasnainhkhan <hhk2170@gmail.com>
2025-03-12 06:19:17 +0530

    aleart message added

--- Smart_Contact_manager/src/main/java/com/smart/contact/controller/AdminController.java
+++ Smart_Contact_manager/src/main/java/com/smart/contact/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.smart.contact.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	
 	@RequestMapping("/index")
-	public String dashboard() {
+	public String dashboard(Model model) {
+		model.addAttribute("title", "Admin Index");
 		return "admin/index";
 	}
 }
